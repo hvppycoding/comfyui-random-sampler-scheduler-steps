452929126a9c81eadde961420bf944dc73baff57
hvppycoding <116823233+hvppycoding@users.noreply.github.com>
2025-06-05 00:00:26 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,12 @@
 
+from .random_sampler_scheduler_steps import RandomSamplerSchedulerSteps
+
+NODE_CLASS_MAPPINGS = {
+    "RandomSamplerSchedulerSteps": RandomSamplerSchedulerSteps,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "RandomSamplerSchedulerSteps": "🎲 RandomSamplerSchedulerSteps",
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
